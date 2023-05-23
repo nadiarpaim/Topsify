7810c71567f548b9db250a2e3e3c2cc62434ed84
Nadiar Paim <nadiarpaim@gmail.com>
2023-05-23 13:53:10 +0100

    burger menu & background video & Search Input

--- app.js
+++ app.js
@@ -28,3 +28,20 @@ accordianWrapper.forEach((accordion) => {
     event.target.nextElementSibling.classList.toggle("hidden");
   });
 });
+
+// NAVBAR
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
