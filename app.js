14079758e651872996934b662e5be454e48e5858
Nadiar Paim <nadiarpaim@gmail.com>
2023-05-23 08:29:20 +0100

    accordion modifications

--- app.js
+++ app.js
@@ -2,35 +2,29 @@
 
 const btnScrollToTop = document.querySelector("#btnScrollToTop");
 
-btnScrollToTop.addEventListener("click", function () {
-    window.scrollTo(0, 0);
-  });
-  
-  const scrollButton = () => {
-    if (document.documentElement.scrollTop > 500) {
-      btnScrollToTop.classList.remove("hidden");
-    } else {
-      btnScrollToTop.classList.add("hidden");
-    }
-  };
-  
-  addEventListener("scroll", scrollButton); 
+const scrollButton = () => {
+  if (document.documentElement.scrollTop > 400) {
+    btnScrollToTop.classList.remove("hidden");
+  } else {
+    btnScrollToTop.classList.add("hidden");
+  }
+};
 
+const pageScroll = () => {
+  window.scrollTo(0, 0);
+};
+
+btnScrollToTop.addEventListener("click", pageScroll);
+window.addEventListener("scroll", scrollButton);
 
 // ACCORDION
-const acc = document.querySelector(".accordion");
-const i;
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    const panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    }
+const accordianWrapper = document.querySelectorAll(".accordion-wrapper");
+
+accordianWrapper.forEach((accordion) => {
+  const header = accordion.querySelector(".accordion");
+
+  header.addEventListener("click", (event) => {
+    event.target.nextElementSibling.classList.toggle("hidden");
   });
-}
-
-
+});
